c6aa0691f7f699c888629c8dc55d5e91c5643e1c
THAMIZH <thamizharasan2373@gmail.com>
2021-03-03 12:06:16 +0530

    download pdf design start

---
+++ lib/desktopview/download_pdf/download pdf.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: DownloadPDFAlert(),
+    ),
+  ));
+}
+
+class DownloadPDFAlert extends StatefulWidget {
+  @override
+  _DownloadPDFAlertState createState() => _DownloadPDFAlertState();
+}
+
+class _DownloadPDFAlertState extends State<DownloadPDFAlert> {
+  _downloadAlert({widget, context}) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Container(
+              height: 650,
+              width: 1200,
+              child: widget,
+            ),
+          );
+        });
+  }
+
+  Widget getUserDetails() {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Container(
+                child: Image.network(
+              'mail.svg',
+              fit: BoxFit.contain,
+            )),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      height: 50,
+      width: 300,
+      child: TextButton(
+        child: Text('Download'),
+        onPressed: () {
+          _downloadAlert(widget: getUserDetails(), context: context);
+        },
+      ),
+    ));
+  }
+}
