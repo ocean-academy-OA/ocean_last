bb20fbf45a731dc8ce68c7178e408fc1d217d37b
THAMIZH <thamizharasan2373@gmail.com>
2021-03-17 12:04:30 +0530

    home screen play icon isuee clear

--- lib/desktopview/Components/video_player_screen.dart
+++ lib/desktopview/Components/video_player_screen.dart
@@ -16,6 +16,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
   final _firestore = FirebaseFirestore.instance;
+  bool isPlay = false;
 
   List<String> addvideo = [];
 
@@ -44,6 +45,21 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     });
   }
 
+  bool isMouse = false;
+  onenter(PointerEvent details) {
+    setState(() {
+      isMouse = true;
+      print(details);
+    });
+  }
+
+  onout(PointerEvent details) {
+    setState(() {
+      isMouse = false;
+      print(details);
+    });
+  }
+
   @override
   void initState() {
     _controller = VideoPlayerController.network(
@@ -66,51 +82,133 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Center(
-            child: FutureBuilder(
+        FutureBuilder(
           future: _initializeVideoPlayerFuture,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               // If the VideoPlayerController has finished initialization, use
               // the data it provides to limit the aspect ratio of the video.
-              return AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                // Use the VideoPlayer widget to display the video.
-                child: ClipRRect(
-                  child: VideoPlayer(_controller),
-                  borderRadius: BorderRadius.circular(20),
+              return MouseRegion(
+                onEnter: onenter,
+                onExit: onout,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      // Use the VideoPlayer widget to display the video.
+                      child: ClipRRect(
+                        child: VideoPlayer(_controller),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    AnimatedOpacity(
+                      opacity: isMouse ? 1 : 0,
+                      duration: Duration(seconds: 1),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              height: 110,
+                              width: 110,
+                              decoration: BoxDecoration(
+                                  color: Colors.black38,
+                                  borderRadius: BorderRadius.circular(5)),
+                            ),
+                            IconButton(
+                              iconSize: 100,
+                              highlightColor: Colors.red,
+                              color: Colors.white,
+                              icon: isPlay
+                                  ? Icon(Icons.pause)
+                                  : Icon(Icons.play_arrow),
+                              onPressed: () {
+                                if (_controller.value.isPlaying) {
+                                  setState(() {
+                                    isPlay = false;
+                                    _controller.pause();
+                                  });
+                                } else {
+                                  setState(() {
+                                    isPlay = true;
+                                    _controller.play();
+                                  });
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               );
             } else {
               // If the VideoPlayerController is still initializing, show a
               // loading spinner.
-              return Center(child: CircularProgressIndicator());
+              return LinearProgressIndicator();
             }
           },
-        )),
-        Center(
-            child: RaisedButton(
-          elevation: 0,
-          color: Colors.transparent,
-          textColor: Colors.white,
-          onPressed: () {
-            // Wrap the play or pause in a call to `setState`. This ensures the
-            // correct icon is shown.
-            setState(() {
-              // If the video is playing, pause it.
-              if (_controller.value.isPlaying) {
-                _controller.pause();
-              } else {
-                // If the video is paused, play it.
-                _controller.play();
-              }
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-            size: 120.0,
-          ),
-        ))
+        ),
+        // Center(
+        //     child: RaisedButton(
+        //   elevation: 0,
+        //   color: Colors.transparent,
+        //   textColor: Colors.white,
+        //   onPressed: () {
+        //     // Wrap the play or pause in a call to `setState`. This ensures the
+        //     // correct icon is shown.
+        //     setState(() {
+        //       // If the video is playing, pause it.
+        //       if (_controller.value.isPlaying) {
+        //         _controller.pause();
+        //       } else {
+        //         // If the video is paused, play it.
+        //         _controller.play();
+        //       }
+        //     });
+        //   },
+        //   child: AnimatedOpacity(
+        //     opacity: isMouse ? 1 : 0,
+        //     duration: Duration(seconds: 1),
+        //     child: Padding(
+        //       padding: const EdgeInsets.only(bottom: 20),
+        //       child: Stack(
+        //         alignment: Alignment.center,
+        //         children: [
+        //           Container(
+        //             height: 110,
+        //             width: 110,
+        //             decoration: BoxDecoration(
+        //                 color: Colors.black38,
+        //                 borderRadius: BorderRadius.circular(5)),
+        //           ),
+        //           IconButton(
+        //             iconSize: 100,
+        //             highlightColor: Colors.red,
+        //             color: Colors.white,
+        //             icon: isPlay ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+        //             onPressed: () {
+        //               if (_controller.value.isPlaying) {
+        //                 setState(() {
+        //                   isPlay = false;
+        //                   _controller.pause();
+        //                 });
+        //               } else {
+        //                 setState(() {
+        //                   isPlay = true;
+        //                   _controller.play();
+        //                 });
+        //               }
+        //             },
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ))
       ],
     );
   }
