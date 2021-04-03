02f72013d4a59f90bda53a77001b5c9ab3f7a38c
THAMIZH <thamizharasan2373@gmail.com>
2021-04-03 16:05:15 +0530

    rework OA

--- lib/mobileview/main.dart
+++ lib/mobileview/main.dart
@@ -7,7 +7,59 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   runApp(OceanMobileView());
 }
 
